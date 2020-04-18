df45409e40911f59bdc1fe11486c12163cb1d584
Jan Princ <princu.jan@gmail.com>
2020-04-18 15:31:17 +0200

    Init

---
+++ src/test/java/cz/princudev/fireapp/fireapi/FireApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.princudev.fireapp.fireapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
